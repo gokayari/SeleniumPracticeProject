c8ee781d85779610639776503d63fbd77e794ed4
gokayari <gokayari@gmail.com>
2022-05-04 12:55:28 +0200

    Selenium Day2

---
+++ src/test/java/com/cydeo/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,13 @@
+package com.cydeo.day2_locators_getText_getAttribute;
+
+public class T1_CydeoVerifications {
+}
+/*
+TC #1: Cydeo practice tool verifications
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com
+3. Verify URL contains
+Expected: cydeo
+4. Verify title:
+Expected: Practice
+ */
