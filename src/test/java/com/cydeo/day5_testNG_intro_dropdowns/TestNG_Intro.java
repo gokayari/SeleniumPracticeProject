96466b0881a02e46bc8fae81df573a837e315e10
gokayari <gokayari@gmail.com>
2022-05-16 01:22:51 +0200

    Selenium Day5 - TestNG: Test, BeforeMethod, AfterMethod

---
+++ src/test/java/com/cydeo/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,29 @@
+package com.cydeo.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1 is running...");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running...");
+    }
+
+}
