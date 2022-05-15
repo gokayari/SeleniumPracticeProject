1496e8d41fd03e03e99275c80ef4aa3d3da9302f
gokayari <gokayari@gmail.com>
2022-05-15 14:38:21 +0200

    Selenium Day5 - T3: Radio Button(with method)

--- src/test/java/com/cydeo/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
+++ src/test/java/com/cydeo/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -1,6 +1,7 @@
 package com.cydeo.day5_testNG_intro_dropdowns;
 
 
+import com.cydeo.utilities.HandleWait;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -41,6 +42,17 @@ public class T3_RadioButton_cont {
                 break;
             }
         }
+
+        //4. Verify “Hockey” radio button is selected after clicking. WITH METHOD:
+
+        clickAndVerifyRadioButton(driver, "sport", "hockey");
+        HandleWait.staticWait(1);
+        clickAndVerifyRadioButton(driver,"sport", "football");
+        HandleWait.staticWait(1);
+        clickAndVerifyRadioButton(driver,"color", "yellow");
+
+        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -48,18 +60,28 @@ public class T3_RadioButton_cont {
         }
         driver.close();
 
-        //4. Verify “Hockey” radio button is selected after clicking.
-        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
 
     }
 
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue){
+        List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
+
+        //Loop through the List of WebElement
+        for (WebElement each : radioButtons) {
+            String eachId = each.getAttribute("id");
+            if (eachId.equals(idValue)){
+                each.click();
+                System.out.println(eachId + " is Selected (With method)= " + each.isSelected());
+            }
+        }
+    }
 }
 /*
 Create a utility method to handle above logic. Method name: clickAndVerifyRadioButton Return type: void or boolean
 Method args:
 1. WebDriver
-2. NameattributeasString(forprovidingwhichgroupofradiobuttons)
-3. IdattributeasString(forprovidingwhichradiobuttontobeclicked)
+2. Name attribute as String(for providing which group of radio buttons)
+3. Id attribute as String(for providing which radio button to be clicked)
 Method should loop through the given group of radio buttons. When it finds the matching option, it should click and verify option is Selected.
 Print out verification: true
  */
