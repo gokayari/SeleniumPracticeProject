3a2db5bd1a3a724d9d0b3e8faadd1ceda4bcfe48
gokayari <gokayari@gmail.com>
2022-05-20 16:09:37 +0200

    Selenium Day6 - Iframe (T4)

---
+++ src/test/java/com/cydeo/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -0,0 +1,77 @@
+package com.cydeo.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_IframePractice {
+
+    //TC #4: Iframe practice
+    //1. Create a new class called: T4_Iframes
+
+    WebDriver driver;
+
+    //@BeforeMethod
+    @BeforeClass
+    public void setupClass(){
+        //2. Create new test and make set ups
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //3. Go to: https://practice.cydeo.com/iframe
+        driver.get("https://practice.cydeo.com/iframe");
+
+
+    }
+
+    //@Ignore   //when we want to ignore this test case
+    @Test
+    public void iframe_test(){
+
+        //4. Assert: “Your content goes here.” Text is displayed.
+
+        //We need to switch driver's focus to iframe
+        //option #1- switching to iframe using id attribute value
+        //driver.switchTo().frame("mce_0_ifr");
+
+        //option #2- passing index number of iframe
+        //driver.switchTo().frame(0);
+
+        //option #3- locate as web element and pass in frame() method
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
+
+        //locate the p tag
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+
+        //To be able to verify the header, we must switch back to "main HTML"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        //assertion of header text is displayed or not
+        Assert.assertTrue(headerText.isDisplayed());
+
+
+
+    }
+
+    @AfterClass
+    public void teardownClass(){
+        driver.close();
+    }
+
+
+
+}
