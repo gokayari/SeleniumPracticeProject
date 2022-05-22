ae7ae45ee051df33f6c73a29eccfc65b69bacc78
gokayari <gokayari@gmail.com>
2022-05-22 23:45:31 +0200

    Selenium Day6 - Alert (T2)

--- src/test/java/com/cydeo/day6_alerts_iframes_windows/T1_2_3_Alert_Practices.java
+++ src/test/java/com/cydeo/day6_alerts_iframes_windows/T1_2_3_Alert_Practices.java
@@ -7,12 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class T1_Alert_Practices {
+public class T1_2_3_Alert_Practices {
 
     //TC #1: Information alert practice
 
@@ -29,8 +30,8 @@ public class T1_Alert_Practices {
         driver.get("http://practice.cydeo.com/javascript_alerts");
 
     }
-
-    @Test
+    //TC #1: Information alert practice
+    @Test (priority = 1)
     public void alert_Test(){
         //3. Click to “Click for JS Alert” button
         WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
@@ -55,7 +56,31 @@ public class T1_Alert_Practices {
         String actualText = resultText.getText();
 
         Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
+    }
 
+    //TC #2: Confirmation alert practice
+    @Test (priority = 2)
+    public void alertTest2(){
+        //3. Click to “Click for JS Confirm” button
+        WebElement confirmationAlertButton = driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+        confirmationAlertButton.click();
 
+        Alert alert = driver.switchTo().alert();
+
+        HandleWait.staticWait(1);
+        //4. Click to OK button from the alert
+        alert.accept();
+
+        //5. Verify “You clicked: Ok” text is displayed.
+        String expectedText = "You clicked: Ok";
+        String actualText = driver.findElement(By.xpath("//p[.=\"You clicked: Ok\"]")).getText();
+
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+    @AfterMethod
+    public void teardownMethod(){
+        HandleWait.staticWait(1);
+        driver.close();
     }
 }
