38790edbc31b8c1d3e92df1a565b1a02a05b8dc1
gokayari <gokayari@gmail.com>
2022-05-24 00:59:30 +0200

    Selenium Day7 - Browser Utils (Sleep, Window Handling, VerifyTitle)

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -26,6 +26,13 @@ public class BrowserUtils {
     }
 
 
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
 
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
 
@@ -40,12 +47,21 @@ public class BrowserUtils {
             if (driver.getCurrentUrl().contains(expectedInUrl)){
                 break;
             }
-
-            // Assert:Title contains “expectedInTitle”
-            String actualTitle = driver.getTitle();
-
-            Assert.assertTrue(actualTitle.contains(expectedInTitle));
         }
+        // Assert:Title contains “expectedInTitle”
+        String actualTitle = driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+
+    public static void verifyTitle(WebDriver driver ,String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
     }
 }
 /*
