831f837ea817555b69775c41682246984b03734b
gokayari <gokayari@gmail.com>
2022-05-24 01:20:59 +0200

    Selenium Day7 - Task T3 Crm Login

--- src/test/java/com/cydeo/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,6 +1,7 @@
 package com.cydeo.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.HandleWait;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
@@ -50,5 +51,21 @@ public class T3_CRM_LOGIN {
         //6. Verify title is as expected:
         //Expected: Portal
         BrowserUtils.verifyTitle(driver, "Portal");
+    }
+
+
+    @Test
+    public void crm_login_test_2(){
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver);
+
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "Portal");
+
     }
 }
