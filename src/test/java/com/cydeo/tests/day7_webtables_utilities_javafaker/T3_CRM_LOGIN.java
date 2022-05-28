9c87e4cdf735da0d94227c685759b1689e0d3e99
gokayari <gokayari@gmail.com>
2022-05-28 12:45:07 +0200

    Selenium Day7 - Task T3 CRM Login

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,29 +1,29 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.HandleWait;
-import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class T3_CRM_LOGIN {
+public class T3_CRM_LOGIN extends TestBase {
 
     //TC #3: Login scenario
+
+
+    //after extending the TestBase abstract class we do not need BeforeMethod
+    /*
     //1. Create new test and make setups
     WebDriver driver;
-
     @BeforeMethod
     public void setupMethod(){
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
+    */
 
     @Test
     public void crm_login_test(){
