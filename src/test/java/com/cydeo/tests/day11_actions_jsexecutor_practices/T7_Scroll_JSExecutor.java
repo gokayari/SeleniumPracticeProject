61e323192c251abc4752ba3939f4ebd795da50d4
gokayari <gokayari@gmail.com>
2022-06-09 12:25:55 +0200

    Selenium Day11 - T7 form day10

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practices/T7_Scroll_JSExecutor.java
@@ -0,0 +1,48 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practices;
+
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.HandleWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class T7_Scroll_JSExecutor {
+
+    @Test
+    public void task7_scroll_test(){
+        //TC #7: Scroll using JavascriptExecutor
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large");
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //3- Scroll down to “Cydeo” link
+        HandleWait.staticWait(2);
+
+        js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
+
+        //4- Scroll up to “Home” link
+        HandleWait.staticWait(2);
+
+        js.executeScript("arguments[1].scrollIntoView(true)", cydeoLink, homeLink);
+        //js.executeScript("arguments[0].scrollIntoView(true)", homeLink); //we can also put like this
+
+        //5- Use below provided JS method only
+        //JavaScript method to use : arguments[0].scrollIntoView(true)
+
+    }
+
+    @AfterMethod
+    public void teardownMethod(){
+        HandleWait.staticWait(1);
+        Driver.getDriver().close();
+    }
+}
+
+//Note: You need to locate the links as web elements and pass them as arguments into executeScript() method
